bb36321f5cdf716f33f31f7369e454f91f737960
hwtcnb <hwtcnb@gmail.com>
2022-06-14 02:21:13 +0300

    changed errro catcher

--- weather.js
+++ weather.js
@@ -1,9 +1,9 @@
-import myJson from './cities.json' assert {type: 'json'};
+import cities from './cities.json' assert {type: 'json'};
 
 
 $(function () {
 
-    var availableTags = myJson.map((element) => element.city)
+    var availableTags = cities.map((element) => element.city)
 
     $("#searchField").autocomplete({
         source: availableTags,
@@ -14,40 +14,37 @@ $(function () {
 
 async function currentWeather(city, utc) {
 
-    let [temperature, feelsLike, humidity, pressure, localTime, cityName] = []
-
-    try {
-        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=16727eaef267711b6e1616dd422158e5`)
-            .then((response) => {
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=16727eaef267711b6e1616dd422158e5`)
+        .then((response) => {
+            if (response.status >= 200 && response.status < 300) {
                 return response.json();
-            })
-            .then((data) => {
-                cityName = data.name
-                temperature = Math.ceil(data.main.temp - 271)
-                feelsLike = Math.ceil(data.main.feels_like - 271)
-                humidity = data.main.humidity
-                pressure = data.main.pressure
-                utc.setSeconds(data.timezone + utc.getSeconds())
-                localTime = String(utc).split(' ').slice(0, 5).join(' ');
-                console.log(data);
-            });
-    } catch (error) {
-        document.querySelector('.weather').style.visibility = 'hidden';
-        document.querySelector('#error').innerHTML = 'Error: Wrong name of city'
-        document.querySelector('#error').style.visibility = 'visible'
-        // clearInterval(liveWeather);
-        throw new Error('Wrong city name');
-        
-    }
-
-    document.querySelector('#error').style.visibility = 'hidden'
-    document.querySelector('#city').innerHTML = 'City: ' + cityName
-    document.querySelector('#current-temp').innerHTML = 'Current temperature: ' + temperature + '°C';
-    document.querySelector('#feels-like').innerHTML = 'Feels like: ' + feelsLike + '°C';
-    document.querySelector('#humidity').innerHTML = 'Humidity: ' + humidity + '%';
-    document.querySelector('#pressure').innerHTML = 'Pressure: ' + pressure + ' hPa';
-    document.querySelector('#local-time').innerHTML = 'Local Time: ' + localTime;
-
+            } else {
+                let error = new Error(response.statusText)
+                error.response = response
+                throw error;
+            }
+        })
+        .then((data) => {
+            var cityName = data.name
+            var { temp, humidity, feels_like: feelsLike, pressure } = data.main;
+            temp = Math.ceil(temp - 271);
+            feelsLike = Math.ceil(feelsLike - 271);
+            utc.setSeconds(data.timezone + utc.getSeconds())
+            var localTime = String(utc).split(' ').slice(0, 5).join(' ');
+            document.querySelector('#city').innerHTML = 'City: ' + cityName
+            document.querySelector('#current-temp').innerHTML = 'Current temperature: ' + temp + '°C';
+            document.querySelector('#feels-like').innerHTML = 'Feels like: ' + feelsLike + '°C';
+            document.querySelector('#humidity').innerHTML = 'Humidity: ' + humidity + '%';
+            document.querySelector('#pressure').innerHTML = 'Pressure: ' + pressure + ' hPa';
+            document.querySelector('#local-time').innerHTML = 'Local Time: ' + localTime;
+        })
+        .catch((e) => {
+            document.querySelector('.weather').style.visibility = 'hidden';
+            setTimeout(() => {
+                alert(e.message);
+            }, 500);
+            // clearInterval(liveWeather);
+        })
 }
 
 // let liveWeather;
